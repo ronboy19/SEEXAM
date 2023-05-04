ee202992b3fa2dc8b2d62a39fb4cd7ed4f80f352
exam <exam@Bcalab4-m20>
2023-05-04 09:35:40 +0530

    HII FIRST COMMIT

---
+++ CaseToolExam/src/pack/Book_221.java
@@ -0,0 +1,16 @@
+package pack;
+
+public class Book_221 {
+int page_num;
+int total_pages;
+
+Book_221(int total_pages){
+	this.total_pages=total_pages;
+	this.page_num=1;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
