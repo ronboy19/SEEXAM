26fef7988d31646ee758ce928807babc58e2f08e
ronboy19 <122138372+ronboy19@users.noreply.github.com>
2023-05-04 09:45:37 +0530

    Update Book_221.java

--- CaseToolExam/src/pack/Book_221.java
+++ CaseToolExam/src/pack/Book_221.java
@@ -8,10 +8,7 @@ Book_221(int total_pages){
 	this.total_pages=total_pages;
 	this.page_num=1;
 }
-	int go_forward(){
-	page_num++;
-	return page_num;	
-	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
